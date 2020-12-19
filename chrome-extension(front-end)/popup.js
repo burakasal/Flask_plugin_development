658fce77cba9bed9331d03fca026a10f4bbc8505
burakasal <63615614+burakasal@users.noreply.github.com>
2020-12-19 16:35:29 +0300

    Update popup.js

--- chrome-extension(front-end)/popup.js
+++ chrome-extension(front-end)/popup.js
@@ -6,14 +6,13 @@ $(function(){
 	//Simple txt label for misc info/debug
 	$("#txtStatus").text("Extension is loaded!").css({"color": "green"});
 
+			
 	//Setup event listener on button
 	$("#btnDo").click(function(e){
-		//console.log("btnDo clicked");
-		
-		//get url of current tab
+
 		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    		let taburl = tabs[0].url;
-    		console.log("URL: "+taburl);
+			let taburl = tabs[0].url;
+			console.log("URL: "+taburl);
 
     		// make ajax request to our server
     		$.ajax({
@@ -25,7 +24,34 @@ $(function(){
            			// You will get response from your PHP page (what you echo or print)
            			console.log(response);
            			$("#txtStatus").text("Connection successful to backend!").css({ 'color': 'green'});
-        			$("#txtResponse").text(response.title+","+response.detail).css({ 'color': 'blue'});
+        			$("#txtResponse").text(response.detail).css({ 'color': 'blue'});
+        		},
+        		error: function(jqXHR, textStatus, errorThrown) {
+           			console.log(textStatus, errorThrown);
+           			$("#txtStatus").text("Connection failed to backend!").css({ 'color': 'red'});
+        			$("#txtResponse").text("");
+        		}
+    		});
+
+  		});		
+	});
+	$("#btnNer").click(function(e){
+
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+			let taburl = tabs[0].url;
+			console.log("URL: "+taburl);
+			
+    		// make ajax request to our server
+    		$.ajax({
+        		url: "http://localhost:5000/api/fetch2",
+        		type: "post",
+        		data: taburl,
+        		contentType: "application/json",
+        		success: function (response) {
+           			// You will get response from your PHP page (what you echo or print)
+           			console.log(response);
+           			$("#txtStatus").text("Connection successful to backend!").css({ 'color': 'green'});
+        			$("#txtResponse").text(response.detail).css({ 'color': 'blue'});
         		},
         		error: function(jqXHR, textStatus, errorThrown) {
            			console.log(textStatus, errorThrown);
@@ -37,3 +63,4 @@ $(function(){
   		});		
 	});
 });
+
