22dd1f95061a061b3ba22dcb31595a7467959dcb
burakasal <63615614+burakasal@users.noreply.github.com>
2021-01-10 15:39:59 +0300

    Update popup.js

--- chrome-extension(front-end)/popup.js
+++ chrome-extension(front-end)/popup.js
@@ -1,12 +1,5 @@
 //Initialize jquery for extension load
 $(function(){
-	// Debug
-	console.log("WebExtloaded");
-
-	//Simple txt label for misc info/debug
-	$("#txtStatus").text("Extension is loaded!").css({"color": "green"});
-
-			
 	//Setup event listener on button
 	$("#btnDo").click(function(e){
 
@@ -21,15 +14,13 @@ $(function(){
         		data: taburl,
         		contentType: "application/json",
         		success: function (response) {
-           			// You will get response from your PHP page (what you echo or print)
            			console.log(response);
-           			$("#txtStatus").text("Connection successful to backend!").css({ 'color': 'green'});
-        			$("#txtResponse").text(response.detail).css({ 'color': 'blue'});
-        		},
-        		error: function(jqXHR, textStatus, errorThrown) {
-           			console.log(textStatus, errorThrown);
-           			$("#txtStatus").text("Connection failed to backend!").css({ 'color': 'red'});
-        			$("#txtResponse").text("");
+					$("#txtResponse").text(response.detail).css({ 'color': 'red'});
+					$("#txtResponse1").text(response.detail2).css({ 'color': 'black'});
+					$("#txtResponse2").text(" ").css({ 'color': 'black'});
+					$("#txtResponse3").text(" ").css({ 'color': 'black'});
+					$("#txtResponse4").text(" ").css({ 'color': 'black'});
+					$("#txtResponse5").text(" ").css({ 'color': 'black'});
         		}
     		});
 
@@ -48,20 +39,71 @@ $(function(){
         		data: taburl,
         		contentType: "application/json",
         		success: function (response) {
-           			// You will get response from your PHP page (what you echo or print)
            			console.log(response);
-           			$("#txtStatus").text("Connection successful to backend!").css({ 'color': 'green'});
-					$("#txtResponse").text(response.org).css({ 'color': 'blue'});
-					$("#txtResponse1").text(response.per).css({ 'color': 'blue'});
-					$("#txtResponse2").text(response.loc).css({ 'color': 'blue'});
-        		},
-        		error: function(jqXHR, textStatus, errorThrown) {
-           			console.log(textStatus, errorThrown);
-           			$("#txtStatus").text("Connection failed to backend!").css({ 'color': 'red'});
-        			$("#txtResponse").text("");
+					$("#txtResponse").text(response.org).css({ 'color': 'red'});
+					$("#txtResponse1").text(response.org2).css({ 'color': 'black'});
+					$("#txtResponse2").text(response.per).css({ 'color': 'red'});
+					$("#txtResponse3").text(response.per2).css({ 'color': 'black'});
+					$("#txtResponse4").text(response.loc).css({ 'color': 'red'});
+					$("#txtResponse5").text(response.loc2).css({ 'color': 'black'});
         		}
     		});
+  		});		
+	});
 
+	$("#btnDo2").click(function(e){
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+			let taburl = tabs[0].url;
+			console.log("URL: "+taburl);
+			
+    		// make ajax request to our server
+    		$.ajax({
+        		url: "http://localhost:5000/api/fetch3",
+        		type: "post",
+        		data: taburl,
+        		contentType: "application/json",
+        		success: function (response) {
+           			console.log(response);
+					$("#txtResponse").text(response.detail).css({ 'color': 'black'});
+        		}
+    		});
+  		});		
+	});
+
+	$("#btnDo3").click(function(e){
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+			let taburl = tabs[0].url;
+			console.log("URL: "+taburl);
+			
+    		// make ajax request to our server
+    		$.ajax({
+        		url: "http://localhost:5000/api/fetch4",
+        		type: "post",
+        		data: taburl,
+        		contentType: "application/json",
+        		success: function (response) {
+           			console.log(response);
+					$("#txtResponse").text(response.detail).css({ 'color': 'black'});
+        		}
+    		});
+  		});		
+	});
+	$("#btnDo4").click(function(e){
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+			let taburl = tabs[0].url;
+			console.log("URL: "+taburl);
+			
+    		// make ajax request to our server
+    		$.ajax({
+        		url: "http://localhost:5000/api/fetch5",
+        		type: "post",
+        		data: taburl,
+        		contentType: "application/json",
+        		success: function (response) {
+           			console.log(response);
+					$("#txtResponse").text(response.detail).css({ 'color': 'black'});
+        		}
+    		});
   		});		
 	});
 });
